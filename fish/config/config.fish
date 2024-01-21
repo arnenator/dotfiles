50f740d3cae421513430699240e6f3cd0307d853
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-01-21 13:27:32 +0100

    Setup fish and a root setup fish file (#3)
    * Add missing tools to brewfile
    
    * Add fish configuration
    
    * Add Mac DS_Store file to gitignore
    
    * Move fish config and create setup_fish script
    
    * Add missing shebang on vscode fish script
    
    * Create initial setup script

---
+++ fish/config/config.fish
@@ -0,0 +1,98 @@
+fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
+fish_add_path /opt/homebrew/opt/gnu-sed/libexec/gnubin
+fish_add_path /opt/homebrew/opt/make/libexec/gnubin
+fish_add_path /opt/homebrew/opt/gawk/libexec/gnubin
+fish_add_path /opt/homebrew/opt/grep/libexec/gnubin
+fish_add_path /opt/homebrew/opt/curl/bin
+
+# kubernetes plugin manager
+# fish_add_path ~/.krew/bin
+# Rancher symlinks
+# fish_add_path  ~/.rd/bin
+
+fish_add_path /usr/local/go/bin/
+fish_add_path /usr/local/bin/
+
+if status is-interactive
+    fish_add_path --prepend ~/bin
+    fish_add_path --prepend ~/go/bin
+    fish_add_path /opt/homebrew/bin
+    eval (/opt/homebrew/bin/brew shellenv)
+
+    fish_add_path '/Applications/Visual Studio Code.app/Contents/Resources/app/bin'
+ 
+    fish_add_path '~/.cargo/env' # Add path to RUST cargo
+
+    set -x EDITOR mg
+
+    set -x LC_ALL en_US.UTF-8
+    set -x LANG en_US.UTF-8
+    set -x ERL_AFLAGS -kernel shell_history enabled
+    set -x LSCOLORS ExGxBxDxCxEgEdxbxgxcxd
+
+    abbr -a gu git pull --rebase --autostash
+    abbr -a gup 'git pull --rebase && git push'
+    abbr -a gp git push
+    abbr -a gpf git push --force-with-lease
+
+    abbr -a gprune git remote prune origin
+
+    abbr -a gs git status
+    abbr -a gsw git switch
+    abbr -a gd git diff
+    abbr -a ga git add
+    abbr -a gap git add -p
+    abbr -a gci git commit
+    abbr -a gcim --set-cursor=@ git commit -m \'@\'
+    abbr -a gcia git commit -a
+    abbr -a gciam --set-cursor=@ git commit -a -m \'@\'
+
+    abbr -a gco git checkout
+    abbr -a gcob git checkout -b
+    abbr -a gcom git checkout master
+    abbr -a gl git log
+    abbr -a gls git log --stat
+    abbr -a glp git log -p
+    abbr -a gbd git pull --prune
+
+    abbr -a k kubectl
+    abbr -a kg kubectl get
+    abbr -a kgns kubectl get ns
+    abbr -a kgnsf kubectl get ns -l product=forecasting
+    abbr -a kga kubectl get all
+    abbr -a kgc kubectl get cronjobs
+    abbr -a kgd kubectl get deployments
+    abbr -a kgr kubectl get replicasets
+    abbr -a kgj kubectl get jobs
+    abbr -a kgp kubectl get pods
+    abbr -a kgpj kubectl get pippijobs
+    abbr -a kgpjr kubectl get pippijobruns
+
+
+    abbr -a kd kubectl describe
+    abbr -a kdns kubectl describe ns
+    abbr -a kdc kubectl describe cronjob
+    abbr -a kdd kubectl describe deployment
+    abbr -a kdr kubectl describe replicaset
+    abbr -a kdj kubectl describe job
+    abbr -a kdp kubectl describe pod
+    abbr -a kdpj kubectl describe pippijob
+    abbr -a kdpjr kubectl describe pippijobruns
+
+
+    abbr -a kdel kubectl delete
+    abbr -a kdelns kubectl delete ns
+    abbr -a kdelc kubectl delete cronjob
+    abbr -a kdeld kubectl delete deployment
+    abbr -a kdelr kubectl delete replicaset
+    abbr -a kdelj kubectl delete job
+    abbr -a kdelp kubectl delete pod
+    abbr -a kdelpj kubectl delete pippijob
+    abbr -a kdelpjr kubectl delete pippijobruns
+
+
+    abbr -a klogs kubectl logs
+    abbr -a klogsf kubectl logs -f
+    abbr -a klogscontainer kubectl logs -f -c container_name
+
+end
---
+++ fish/setup_fish.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+echo "Configuring fish..."
+
+cp ./config ~/.config/fish
