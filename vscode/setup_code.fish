be9fe0d9538e55234d8bad68ea62836bdad2c9cf
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-01-24 14:53:22 +0100

    Fix overwrite and copying of directory issues

--- vscode/setup_code.fish
+++ vscode/setup_code.fish
@@ -6,10 +6,10 @@ set ext_categories language theme tool
 
 # Language extensions
 for category in $ext_categories
-    for ext_name in (cat "./$category.extensions")
+    for ext_name in (cat "./extensions/$category.extensions")
         code --install-extension $ext_name
     end
 end
 
 echo "Setting User Configuration..."
-cp ./settings/user.settings.json ~/Library/Application Support/Code/User/settings.json
+cp ./settings/user.settings.json ~/Library/Application\ Support/Code/User/settings.json
